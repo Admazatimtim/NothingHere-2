6bef2cdcd4ed25f76411b814fed5d04132da9156
Tillfore <tillfore@foxmail.com>
2018-03-09 08:48:00 +0800

    renamed:    Practice/testGUI.cs -> Practice/testGUI[Unity]/testGUI.cs 	renamed:    Practice/testGUILayout.cs -> Practice/testGUI[Unity]/testGUILayout.cs 	new file:   Practice/testGUI[Unity]/testGUISkin.cs 	new file:   Practice/testGUI[Unity]/testGUISkin.guiskin

--- Practice/testGUI[Unity]/testGUILayout.cs
+++ Practice/testGUI[Unity]/testGUILayout.cs
@@ -23,10 +23,10 @@ public class testGUILayout : MonoBehaviour {
         GUILayout.BeginArea(areaA);//开始一个区域，区域内的GUILayout会在选项的基础上，随着区域大小、位置的变化，动态变化
         //GUILayout可以配置多个GUILayoutOption[]参数
         GUILayout.Button("设置布局按钮宽度为300，高度为30", GUILayout.Width(300), GUILayout.Height(30));
-        GUILayout.FlexibleSpace();//创建自适应间距（使得最上和最下的控件分别置顶置底）
+        GUILayout.Space(10);//创建间距=10
         GUILayout.Button("设置布局按钮最小宽度为100，最小高度为20", GUILayout.MinWidth(100), GUILayout.MinHeight(20));
         GUILayout.Button("设置布局按钮最大宽度为400，最大高度为40", GUILayout.MaxWidth(400), GUILayout.MaxHeight(40));
-        GUILayout.FlexibleSpace();//创建自适应间距
+        GUILayout.FlexibleSpace();//创建自适应间距（使得最上和最下的控件分别置顶置底）
         GUILayout.Button("设置宽度不等于最宽按钮", GUILayout.ExpandWidth(false));
         GUILayout.Button("设置宽度等于最宽按钮", GUILayout.ExpandWidth(true));
         GUILayout.EndArea();
---
+++ Practice/testGUI[Unity]/testGUISkin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testGUISkin : MonoBehaviour {
+    /*
+     * 运用GUISkin和GUIStyle，对前面的GUI、GUILayout等进行梳理。 
+     * 
+     */
+    public GUISkin myGUISkin;
+    public GUIStyle theGUIStyle;
+
+    private Rect windowRect;
+
+    void Start() {
+        windowRect = new Rect(0, 0, 300, 500);
+    }
+
+    void OnGUI() {
+        GUI.skin = myGUISkin;
+        // static UnityEngine.Rect Window(Int32 id, UnityEngine.Rect clientRect, WindowFunction func, UnityEngine.GUIContent title, UnityEngine.GUIStyle style)
+        // [窗口内容输出目标] = GUI.Window([窗口序号],[窗口空间信息（输入）],[窗口函数],[窗口标题名称]，[GUI样式（默认或已在上面声明)])
+        windowRect = GUI.Window(16, windowRect, WindowFunc, "GUI样式测试窗口");
+    }
+
+    void WindowFunc(int winID) {
+        GUI.DragWindow();//声明窗口允许拖动
+
+        GUILayout.BeginVertical();
+        GUILayout.Space(8);
+        GUILayout.Label("这个Label用了Button样式", "Button");
+        GUILayout.Button("这个Button用了GUISkin中的自定义样式", "testGUIStyle");
+        GUILayout.FlexibleSpace();//创建自适应间距（使得最上和最下的控件分别置顶置底）
+        GUILayout.Box("这个Button用了自设置样式theGUIStyle", theGUIStyle);
+        GUILayout.EndVertical();
+    }
+}
