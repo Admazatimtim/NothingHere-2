4078e1e3bc1ceacb94f4e6d79be6c631bc5f19c3
Tillfore <tillfore@foxmail.com>
2018-03-12 00:00:32 +0800

    new file:   Practice/testErrorCheck[Unity]/testCallStack.cs

---
+++ Practice/testErrorCheck[Unity]/testCallStack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testCallStack : MonoBehaviour {
+	
+    private float _mCount = 0;
+    
+	void Update () {
+        _mCount += 1 * Time.deltaTime;
+        if (_mCount >= 30) {
+            FunctionA();
+        }  
+    }
+
+    void FunctionA() {
+        _mCount = 0;
+        FunctionB();
+    }
+
+    void FunctionB() {
+        string message = _mCount.ToString();
+        FunctionC(message);
+    }
+
+    void FunctionC(string message) {
+        GUILayout.TextArea("数字是："+message, 200);
+    }
+}
